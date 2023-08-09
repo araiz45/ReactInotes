a1029663d8dc9a14902929eab432ac470d00c023
araiz45 <araizzahid45@gmail.com>
2023-08-10 02:10:17 +0500

    video 57 completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
-
+import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+        <Navbar />
+        <Home />
+        </>
+      ),
+    },
+    {
+      path: "about",
+      element: ( 
+      <>
+        <Navbar/>
+        <About/>
+      </>)
+    },
+  ]);  
   return (
-    <h1>hello from inotes</h1>
+    <>
+      <RouterProvider exact router={router} />
+    </>
   );
 }
 
